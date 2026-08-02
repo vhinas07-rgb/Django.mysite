73c592b9122bc2f3980dbc81b7faf158055035cd
vhinas07 <vhinas07@gmail.com>
2026-08-02 09:48:27 +0330

    Add static files, update urls/settings, remove pycache from tracking

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Anasite.apps.AnasiteConfig'
+    'Anasite.apps.AnasiteConfig',
+
 ]
 
 MIDDLEWARE = [
--- mysite/urls.py
+++ mysite/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path ,include
+from django.urls import path , include
 
-urlpatterns = [
+urlpatterns =[
     path('admin/', admin.site.urls),
-    # path ('url  address', 'view')
     path('',include('Anasite.urls')),
-    #path('blog/',include('blog.urls'))
-]
+
+    #  path('blog/',include('blog.urls'))
+                 ]
