11922df2d70f0c7a2cc2dcb4a616b3b482ecb4d0
vhinas07 <vhinas07@gmail.com>
2026-08-19 21:33:27 +0330

    +Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+
+counted_views = models.IntegerField(default=0)
+status = models.BooleanField()
+published_date = models.DateTimeField()
+created_date = models.DateField(auto_now_add=True)
+updated_date = models.DateTimeField(auto_now=True)
+# published_date = models.DateTimeFiled()
