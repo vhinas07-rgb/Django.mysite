e847d8f402714a50f54c59494a00c829cfbd4aaf
vhinas07 <vhinas07@gmail.com>
2026-08-22 11:01:34 +0330

    +Models

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 
 # Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
 
+# image =
+# author = 
+title = models.CharField(max_length=255)
+content = models.TextField()
+# tags = 
+# category = 
 counted_views = models.IntegerField(default=0)
-status = models.BooleanField()
-published_date = models.DateTimeField()
-created_date = models.DateField(auto_now_add=True)
+status = models.BooleanField(default=False)
+published_date = models.DateTimeField(null=True)
+created_date = models.DateTimeField(auto_now_add=True)
 updated_date = models.DateTimeField(auto_now=True)
-# published_date = models.DateTimeFiled()
+#  / مدل فعلی Contact  
+class Contact(models.Model):
+    name = models.CharField(max_length=255)
+email = models.EmailField()
+subject = models.CharField(max_length=255)
+message = models.TextField()
+created_date = models.DateTimeField(auto_now_add=True)
+updated_date = models.DateTimeField(auto_now=True)
